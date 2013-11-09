e29505c34e216fd3f7d027513888ccb2f689466d
Bob <bob.buehler@gmail.com>
2013-11-09 16:24:23 -0600

    ForEach extension

--- VS2010/CSClient/Extensions.cs
+++ VS2010/CSClient/Extensions.cs
@@ -4,36 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Collections;
 
-public static class Extensions
+namespace CSClient
 {
-    public static T minByValue<T, V>(this IEnumerable<T> source, Func<T, V> selector)
+    public static class Extensions
     {
-        var comparer = Comparer<V>.Default;
-        T first = source.First();
-        T min = first;
-        V minValue = selector(first);
-        foreach (T s in source)
+        public static T minByValue<T, V>(this IEnumerable<T> source, Func<T, V> selector)
         {
-            V value = selector(s);
-            if (comparer.Compare(value, minValue) < 0)
+            var comparer = Comparer<V>.Default;
+            T first = source.First();
+            T min = first;
+            V minValue = selector(first);
+            foreach (T s in source)
             {
-                min = s;
-                minValue = value;
+                V value = selector(s);
+                if (comparer.Compare(value, minValue) < 0)
+                {
+                    min = s;
+                    minValue = value;
+                }
             }
+            return min;
         }
-        return min;
-    }
 
-    public static bool Get(this BitArray b, Point p)
-    {
-        return b.Get(Bb.GetOffset(p.x, p.y));
-    }
-
-    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
-    {
-        foreach (var s in source)
+        public static bool Get(this BitArray b, Point p)
         {
-            action(s);
+            return b.Get(Bb.GetOffset(p.x, p.y));
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var s in source)
+            {
+                action(s);
+            }
         }
     }
 }
