8ea0b5b710c00cf8543718c06aac99e89c150cf6
Bob <bob.buehler@gmail.com>
2013-11-09 13:55:30 -0600

    Added Point.cs

---
+++ VS2010/CSClient/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSClient
+{
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Y * 20 + X;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Point other = (Point)obj;
+            return (X == other.X) && (Y == other.Y);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0},{1})", X, Y);
+        }
+    }
+}
