abccdd519e0e10105271c730bd0c5fd589b3f73f
Jared <jjbvfb@mst.edu>
2013-11-09 14:40:39 -0600

    Updated bit boards

--- VS2010/CSClient/Bb.cs
+++ VS2010/CSClient/Bb.cs
@@ -6,10 +6,11 @@ using System.Collections;
 
 namespace CSClient
 {
+    // Pump station is multiple tiles, need list of pump station bitboards
     /* Bit Boards */
     public static class Bb
     {
-        public static int maxX;
+        public static int maxX; // (0, 0) is the top-left corner
         public static int maxY;
         public static BitArray Glaciers;
         public static BitArray Trenches;
@@ -24,15 +25,57 @@ namespace CSClient
         {
             maxX = ai.mapWidth();
             maxY = ai.mapHeight();
+            int size = maxX * maxY;
+            int us = ai.playerID();
+            int them = 1-ai.playerID();
 
-            Glaciers = new BitArray(maxX * maxY);
-            Trenches = new BitArray(maxX * maxY);
-            OurPumps = new BitArray(maxX * maxY);
-            TheirPumps = new BitArray(maxX * maxY);
-            Pumps = new BitArray(maxX * maxY);
-            OurSpawns = new BitArray(maxX * maxY);
-            TheirSpawns = new BitArray(maxX * maxY);
-            Water = new BitArray(maxX * maxY);
+            Glaciers = new BitArray(size);
+            Trenches = new BitArray(size);
+            OurPumps = new BitArray(size);
+            TheirPumps = new BitArray(size);
+            Pumps = new BitArray(size);
+            OurSpawns = new BitArray(size);
+            TheirSpawns = new BitArray(size);
+            Water = new BitArray(size);
+
+            foreach (Tile tile in BaseAI.tiles)
+            {
+                int offset = GetOffset(tile.X, tile.Y);
+                // Trenches
+                if (tile.Depth > 0)
+                {
+                    Trenches[offset] = true;
+                }
+                // Water
+                if (tile.WaterAmount > 0)
+                {
+                    Water[offset] = true;
+                }
+                // OurPumps, TheirPumps
+                if (tile.PumpID == us)
+                {
+                    OurPumps[offset] = true;
+                }
+                else if (tile.PumpID == them)
+                {
+                    TheirPumps[offset] = true;
+                }
+                // OurSpawns, TheirSpawns
+                if (tile.Owner == us)
+                {
+                    OurSpawns[offset] = true;
+                }
+                else if(tile.Owner == them)
+                {
+                    TheirSpawns[offset] = true;
+                }
+                
+            }
+        }
+
+        public static int GetOffset(int x, int y)
+        {
+            return y * maxX + x;
         }
     }
 }
