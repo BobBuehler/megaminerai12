70bc8aec5c10238c16ff1d76c364efa1039f1d8a
Jared <jjbvfb@mst.edu>
2013-11-09 17:37:18 -0600

    Updated Bb.cs with additional functionality

--- Bb.cs
+++ Bb.cs
@@ -12,41 +12,125 @@ public static class Bb
 {
     public static int maxX; // (0, 0) is the top-left corner
     public static int maxY;
+    public static int us;
+    public static int them;
+
     public static BitArray OurPumps;
     public static BitArray TheirPumps;
     public static BitArray NeutralPumps;
+    public static BitArray OurTiles;
+    public static BitArray TheirTiles;
     public static BitArray OurSpawns;
     public static BitArray TheirSpawns;
     public static BitArray Glaciers;
     public static BitArray Trenches;
     public static BitArray Water;
+    public static BitArray OurUnits;
+    public static BitArray TheirUnits;
 
     public static HashSet<Point> OurPumpSet;
     public static HashSet<Point> TheirPumpSet;
-    public static HashSet<Point> NeutralPumpSet;
+    public static HashSet<Point> NeutralPumpSet; // May be able to ignore?
     public static HashSet<Point> OurSpawnSet;
     public static HashSet<Point> TheirSpawnSet;
     public static HashSet<Point> GlaciersSet;
     public static HashSet<Point> TrenchesSet;
     public static HashSet<Point> WaterSet;
 
-    public static HashSet<Unit> OurUnits;
-    public static HashSet<Unit> TheirUnits;
-    public static HashSet<Unit> OurWorkers;
-    public static HashSet<Unit> TheirWorkers;
-    public static HashSet<Unit> OurScouts;
-    public static HashSet<Unit> TheirScouts;
-    public static HashSet<Unit> OurTanks;
-    public static HashSet<Unit> TheirTanks;
+    public static HashSet<Unit> OurUnitsSet;
+    public static HashSet<Unit> TheirUnitsSet;
+    public static HashSet<Unit> OurWorkersSet;
+    public static HashSet<Unit> TheirWorkersSet;
+    public static HashSet<Unit> OurScoutsSet;
+    public static HashSet<Unit> TheirScoutsSet;
+    public static HashSet<Unit> OurTanksSet;
+    public static HashSet<Unit> TheirTanksSet;
 
     public static void Init(AI ai)
     {
         maxX = ai.mapWidth();
         maxY = ai.mapHeight();
-        int size = maxX * maxY;
-        int us = ai.playerID();
-        int them = 1 - ai.playerID();
+        us = ai.playerID();
+        them = 1 - ai.playerID();
 
+        Reset();
+
+        ReadBoard();
+    }
+
+    public static void ReadBoard()
+    {
+        Reset();
+
+        foreach (Tile tile in BaseAI.tiles)
+        {
+            int offset = GetOffset(tile.X, tile.Y);
+            Point point = new Point(tile.X, tile.Y);
+
+            // Trenches
+            if (tile.Depth > 0 && tile.WaterAmount == 0)
+            {
+                Trenches[offset] = true;
+                TrenchesSet.Add(point);
+            }
+
+            // Water
+            if (tile.WaterAmount > 0 && tile.Owner != 3 && tile.Depth > 0)
+            {
+                Water[offset] = true;
+                WaterSet.Add(point);
+            }
+
+            // Glaciers
+            if (tile.Depth == 0 && tile.WaterAmount > 0 && tile.Owner == 3)
+            {
+                Glaciers[offset] = true;
+                GlaciersSet.Add(point);
+            }
+
+            // Pumping Stations
+            if (tile.PumpID != -1)
+            {
+                foreach (PumpStation station in BaseAI.pumpStations)
+                {
+                    if (station.Id == tile.PumpID)
+                    {
+                        if (station.Owner == us)
+                        {
+                            OurPumps[offset] = true;
+                            OurPumpSet.Add(point);
+                        }
+                        else if (station.Owner == them)
+                        {
+                            TheirPumps[offset] = true;
+                            TheirPumpSet.Add(point);
+                        }
+                        else
+                        {
+                            NeutralPumps[offset] = true;
+                            NeutralPumpSet.Add(point);
+                        }
+                    }
+                }
+            }
+
+            // Spawn Tiles
+            if (tile.Owner == us)
+            {
+                OurTiles[offset] = true;
+            }
+            else if (tile.Owner == them)
+            {
+                TheirTiles[offset] = true;
+            }
+
+            OurSpawns = OurTiles.Or(OurPumps).And(TheirUnits.Not()).And(OurUnits.Not());
+        }
+    }
+
+    private static void Reset()
+    {
+        int size = maxX * maxY;
         Glaciers = new BitArray(size);
         Trenches = new BitArray(size);
         OurPumps = new BitArray(size);
@@ -55,6 +139,8 @@ public static class Bb
         OurSpawns = new BitArray(size);
         TheirSpawns = new BitArray(size);
         Water = new BitArray(size);
+        OurUnits = new BitArray(size);
+        TheirUnits = new BitArray(size);
 
         OurPumpSet = new HashSet<Point>();
         TheirPumpSet = new HashSet<Point>();
@@ -65,90 +151,14 @@ public static class Bb
         TrenchesSet = new HashSet<Point>();
         WaterSet = new HashSet<Point>();
 
-        OurUnits = new HashSet<Unit>();
-        TheirUnits = new HashSet<Unit>();
-        OurWorkers = new HashSet<Unit>();
-        TheirWorkers = new HashSet<Unit>();
-        OurScouts = new HashSet<Unit>();
-        TheirScouts = new HashSet<Unit>();
-        OurTanks = new HashSet<Unit>();
-        TheirTanks = new HashSet<Unit>();
-
-        foreach (Tile tile in BaseAI.tiles)
-        {
-            int offset = GetOffset(tile.X, tile.Y);
-            Point point = new Point(tile.X, tile.Y);
-            // Trenches
-            if (tile.Depth > 0)
-            {
-                Trenches[offset] = true;
-                TrenchesSet.Add(point);
-            }
-            // Water
-            if (tile.WaterAmount > 0)
-            {
-                Water[offset] = true;
-                WaterSet.Add(point);
-            }
-            // OurPumps, TheirPumps
-            if (tile.PumpID == us)
-            {
-                OurPumps[offset] = true;
-                OurPumpSet.Add(point);
-                // Add something to the spawnset
-            }
-            else if (tile.PumpID == them)
-            {
-                TheirPumps[offset] = true;
-                TheirPumpSet.Add(point);
-                // Add something to the spawnset
-            }
-            else if (tile.PumpID != -1)
-            {
-                NeutralPumps[offset] = true;
-                NeutralPumpSet.Add(point);
-            }
-            // OurSpawns, TheirSpawns
-            if (tile.Owner == us)
-            {
-                OurSpawns[offset] = true;
-                OurSpawnSet.Add(point);
-            }
-            else if (tile.Owner == them)
-            {
-                TheirSpawns[offset] = true;
-                TheirSpawnSet.Add(point);
-            }
-            if (tile.Owner == 3 && tile.WaterAmount > 0)
-            {
-                Glaciers[offset] = true;
-            }
-        }
-
-
-    }
-
-    public static void ReadBoard()
-    {
-        foreach (Tile tile in BaseAI.tiles)
-        {
-            int offset = GetOffset(tile.X, tile.Y);
-            Point point = new Point(tile.X, tile.Y);
-
-            // Trenches
-            if (tile.Depth > 0)
-            {
-                Trenches[offset] = true;
-                TrenchesSet.Add(point);
-            }
-
-            // Water
-            if (tile.WaterAmount > 0 && tile.Owner != 3)
-            {
-                Water[offset] = true;
-                WaterSet.Add(point);
-            }
-        }
+        OurUnitsSet = new HashSet<Unit>();
+        TheirUnitsSet = new HashSet<Unit>();
+        OurWorkersSet = new HashSet<Unit>();
+        TheirWorkersSet = new HashSet<Unit>();
+        OurScoutsSet = new HashSet<Unit>();
+        TheirScoutsSet = new HashSet<Unit>();
+        OurTanksSet = new HashSet<Unit>();
+        TheirTanksSet = new HashSet<Unit>();
     }
 
     public static int GetOffset(int x, int y)
@@ -156,7 +166,7 @@ public static class Bb
         return y * maxX + x;
     }
 
-    public static bool IsPumping(this PumpStation station)
+    public static bool IsPumping(Point m)
     {
         //station
         throw new NotImplementedException();
@@ -164,4 +174,3 @@ public static class Bb
 
     // Is this pump pumping; start = every tile under pump; isGoal = is it a glacier?; passable = pump OR water OR glacier; c: (c, n) => 1; h: c => 0
 }
-
