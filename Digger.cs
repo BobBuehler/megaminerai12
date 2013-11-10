f6aa984c2b36d17a39d5c9e35ab04a268155cb5b
Bob <bob.buehler@gmail.com>
2013-11-09 23:31:55 -0600

    Digger fix

--- Digger.cs
+++ Digger.cs
@@ -6,7 +6,7 @@ using System.Collections;
 
 public static class Digger
 {
-    public IEnumerable<Point> GetNeededTrenches(Pump pump)
+    public static IEnumerable<Point> GetNeededTrenches(Pump pump)
     {
         var starts = new HashSet<Point>(pump.GetPoints());
         var goals = Bb.GlaciersSet;
@@ -14,4 +14,34 @@ public static class Digger
         var path = Pather.AStar(starts, p => goals.Contains(p), impassable.Not(), (c, n) => Bb.Water.Get(n) ? 0 : 1, p => 0);
         return path.Where(p => !(Bb.Water.Get(p) || starts.Contains(p) || goals.Contains(p)));
     }
+
+    //public static void Dig(Unit digger, IEnumerable<Point> targets)
+    //{
+    //    Pather.GetNeighbors(digger.ToPoint(), 
+    //}
+
+    //public static void MoveAndDig(Unit digger, IEnumerable<Point> targets)
+    //{
+    //    var diggable = 
+    //    Pather.GetNeighbors(
+    //    if (Attack(attacker, targets))
+    //    {
+    //        return;
+    //    }
+
+    //    if (attacker.MovementLeft == 0)
+    //    {
+    //        return;
+    //    }
+
+    //    var steps = GetWalkingSteps(attacker.ToPoint(), targets.Select(t => t.ToPoint()).ToBitArray());
+    //    foreach (var step in steps)
+    //    {
+    //        attacker.move(step.x, step.y);
+    //        if (Attack(attacker, targets) || attacker.MovementLeft == 0)
+    //        {
+    //            return;
+    //        }
+    //    }
+    //}
 }
